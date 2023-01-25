984dac21c1e2b0ce0d9996d76e732bf4bf593cbe
L0khm4nR4h1m <2020834578@student.uitm.edu.my>
2023-01-25 19:05:00 +0800

    lola

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://CaptainN3m0:Lu%40834578@cluster1.5nmlooo.mongodb.net/test', {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
